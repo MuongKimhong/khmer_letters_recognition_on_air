fe72258f083b6ef8a6212c498626b9fce5fd3640
muongkimhong <muongkimhong086@gmail.com>
2021-06-09 15:10:18 +0700

    implemented interface

--- interface/index.js
+++ interface/index.js
@@ -1,2 +1,45 @@
 console.log("[INFO] Starting khmer letters recognition application...")
 
+const { exec }   = require("child_process")
+const electron   = require('electron')
+const { stdout } = require("process")
+const path       = require('path')
+const url        = require('url')
+
+const { app, BrowserWindow, ipcMain } = electron
+
+var interfaceWindow
+
+// Listen to app ready event
+app.on('ready', () => {
+    interfaceWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration : true,
+            contextIsolation: false
+        }
+    })
+    // load html
+    interfaceWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes : true
+    }))
+})
+
+ipcMain.on('executeDataCollectingModule', () => {
+    exec("source/env/bin/python3.8 source/app.py --data True", (error, stdout, stderr) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+        if (stderr) {
+            console.log(stderr)
+            return
+        }
+        else console.log("[INFO] Successfully connected to data collecting module")
+    })
+})
+
+ipcMain.on('executePredictModule', () => {
+    console.log("Not done yet")
+})
